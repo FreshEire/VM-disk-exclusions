3a85fcbbe59393f28140c42c29c118829a03061f
Patrick Codd <kod3sign@gmail.com>
2023-03-03 15:23:45 +0000

    modified:   vmdiskcheck.py

--- vmdiskcheck.py
+++ vmdiskcheck.py
@@ -3,10 +3,15 @@
 import requests
 import json
 import urllib3
+import datetime
 
 # NOTE: this disables warnings for insecure SSL/TLS connections to maintain clean terminal output for testing purposes only.
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# Create a unique filename based on the current date and time
+result_text = "vmDiskExclusions_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
+
+# User input
 rubrik_ip=input("Please enter Rubrik node IP: ")
 user_id=input("Please enter User ID: ")
 user_secret=input("Please enter Secret: ")
@@ -27,7 +32,7 @@ try:
     open_session_response = requests.post(open_session_url, headers=open_session_headers, json=open_session_data, verify=False)
     if open_session_response.status_code == 200:
         user_token = open_session_response.json().get('token')
-        print(f"\nToken for user is: \n{user_token}\nThis will be vlid for 24 hours or until session is closed\n")
+        print(f"\nToken for user is: \n{user_token}\nThis will be valid for 24 hours or until session is closed\n")
     else:
         print(f"Request failed with status code {open_session_response.status_code}")
 except Exception as e:
@@ -46,11 +51,24 @@ try:
     vm_details_reponse = requests.get(vms_list_url, headers=vms_list_headers, verify=False)
     if vm_details_reponse.status_code == 200:
         vm_details_json = json.loads(vm_details_reponse.content.decode('utf-8'))['data']
-        for each_vm in vm_details_json:
-            current_vm = dict(each_vm)
+        
+        for each_vm in vm_details_json:    
             
-            # Get VM info for each VM in the dict
+            current_vm = dict(each_vm)
+            vm_output = {}
+            
+            # Get VM info for each VM in the response
             vm_id = current_vm["id"]
+            vm_name = current_vm["name"]
+            
+            # Build new dictionary per VM
+            vm_output["name"] = vm_name
+            vm_output["vmId"] = vm_id
+            disk_counter = 0
+            excluded_disk_counter = 0
+            
+                    
+            print(f"Checking VM: " + vm_name + " | " + vm_id + "\n")
             
             vm_info_url = f"https://{rubrik_ip}/api/v1/vmware/vm/{vm_id}"
             vm_info_headers = {
@@ -61,12 +79,13 @@ try:
             vm_info_reponse = requests.get(vm_info_url, headers=vm_info_headers, verify=False)
             vm_info_json = json.loads(vm_info_reponse.content.decode('utf-8'))
             
-            # Get disk info for each disk per VM iteration
-            vm_disks = dict(vm_info_json)["virtualDiskIds"]    
-            #print(str(vm_disks) + "\n")
+            # Get disk info for each disk per VM iteration from the response above
+            vm_disks = dict(vm_info_json)["virtualDiskIds"]
+            
             for each_disk in list(vm_disks):
-                #print(each_disk)
-                            
+                disk_counter += 1
+                vm_output["diskCount"] = disk_counter
+                
                 vm_disk_url = f"https://{rubrik_ip}/api/v1/vmware/vm/virtual_disk/{each_disk}"
                 vm_disk_headers = {
                     "accept": "application/json",
@@ -75,19 +94,25 @@ try:
                 }
                 vm_disk_reponse = requests.get(vm_disk_url, headers=vm_disk_headers, verify=False)
                 vm_disk_json = json.loads(vm_disk_reponse.content.decode('utf-8'))
-                #print(vm_disk_json)
+                
                 if vm_disk_json["excludeFromSnapshots"]:
-                    print("VM Name: " + current_vm["name"] + "\nVM ID: " + current_vm["id"] + "\nVM disk exclusions:")
-                    print(f"{each_disk}\n")
+                    excluded_disk_counter += 1
+                    vm_output["excludedDiskCount"] = excluded_disk_counter
+                    vm_output[f"excludedDisk{excluded_disk_counter}"] = each_disk
                 else:
                     pass
+                    
+            if excluded_disk_counter > 0:
+                with open(result_text, 'a', encoding='utf-8') as f:
+                    f.write(str(vm_output) + "\n")
     else:
         print(f"Request failed with status code {vm_details_reponse.status_code}: {vm_details_reponse.text}")
 except Exception as e:
     print(f"An error occurred: {e}")
+    print(f"Please close the session manually using curl:")
+    print(f"curl -X DELETE 'https://{rubrik_ip}/api/v1/session/me' -H 'accept: application/json' -H 'Authorization: Bearer {user_token}'")
 
-
-# Close open sessions (Max sessions allowed per service account is 10)
+# Close open session
 close_sessions_url = f"https://{rubrik_ip}/api/v1/session/me"
 close_sessions_headers = {
     "accept": "application/json",
@@ -98,8 +123,9 @@ close_sessions_headers = {
 try:
     close_sessions__response = requests.delete(close_sessions_url, headers=close_sessions_headers, verify=False)
     if close_sessions__response.status_code == 204:
-        print("\nSession was closed successfully")
+        print("Session was closed successfully")
+        print(f"Result of the check can be found in file {result_text}\n")
     else:
-        print(f"\nRequest failed with status code {close_sessions__response.status_code}")
+        print(f"Request failed with status code {close_sessions__response.status_code}")
 except Exception as e:
-    print(f"\nAn error occurred: {e}")
+    print(f"An error occurred: {e}")
